def7c942533389353d6326048382bba2b4b41f17
joseph <josephalittle92@gmail.com>
2020-04-23 21:11:41 -0500

    create html, css, js files.  add skeleton html.  add inital js functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate(operator, num1, num2) {
+    //Create a new function operate that takes an operator 
+    //and 2 numbers and then calls one of the above 
+    //functions on the numbers.
+}
