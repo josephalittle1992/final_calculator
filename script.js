0e22a4df1b432f2f060781e07f20b02445591f06
joseph <josephalittle92@gmail.com>
2020-04-26 13:22:04 -0500

    added more calculator features

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let numBtns = document.querySelectorAll('[data-number]');
+let displaySpan = document.getElementById('display');
+let disUpValue = ''; //displayUpdate() var
+let operating = ''; //stores 1st number of calculation when
+                    //operator user presses '='
+let operator = ''; //operator chosen for calculation
 
-function displayUpdate(value) {
-    let displaySpan = document.getElementById('display');
-    displaySpan.textContent = value;
-}
-
+//number buttons
 numBtns.forEach(btn => {
     btn.addEventListener('click', (_btn) => {
         displayUpdate(btn.innerHTML);
     });
 });
+
+
+//clear button
+document.getElementById('clear').addEventListener('click', () => {
+    disUpValue = '';
+    displaySpan.innerHTML = '';
+});
+
+//lets multiple numbers be added to display span
+function displayUpdate(value) {
+    disUpValue = disUpValue.concat(value);
+    displaySpan.innerHTML = disUpValue;
+}
 
 function add(a, b) {
     return a + b;
@@ -34,8 +48,6 @@ function operate(operator, num1, num2) {
 }
 
 function init() {
-
-    console.log(numBtns);
 
 }
 
